493799c92cf3d94b4b29f168dcc43f1cfd4ae9dd
Karen <karen_carvalho04@hotmail.com>
2025-11-27 16:04:35 -0300

    autent google

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { AuthProvider } from "./context/AuthContext.jsx";
-
+import { AuthProvider } from "./Context/AuthContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
